a9ac3e423c6b6bddee1378d194008ff4c8ffb110
monicaleep <monica.parrillo@gmail.com>
2020-08-25 14:34:20 -0500

    Updates

---
+++ precache-manifest.1279ebbcf23b00d94072e128168c9406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61d8a7ba2934f9e380cec9dc8c6cb4a",
+    "url": "/MarkdownPreviewer/index.html"
+  },
+  {
+    "revision": "d7c772c07b7b9d3fc6c4",
+    "url": "/MarkdownPreviewer/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "e8a55507a98d77c5b703",
+    "url": "/MarkdownPreviewer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7c772c07b7b9d3fc6c4",
+    "url": "/MarkdownPreviewer/static/js/2.50334c81.chunk.js"
+  },
+  {
+    "revision": "044fa327d8ec9501b1aca3401be13b99",
+    "url": "/MarkdownPreviewer/static/js/2.50334c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a55507a98d77c5b703",
+    "url": "/MarkdownPreviewer/static/js/main.9c6cc89c.chunk.js"
+  },
+  {
+    "revision": "9f72143d1add77a956d2",
+    "url": "/MarkdownPreviewer/static/js/runtime-main.787f1e30.js"
+  }
+]);
